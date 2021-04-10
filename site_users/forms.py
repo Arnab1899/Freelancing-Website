49bf204526cd13b469ad38171d2aea62752e3733
Protiva Arafin <protivaharafin@gmail.com>
2021-04-10 14:22:25 +0600

    Integrity Error Solved

--- site_users/forms.py
+++ site_users/forms.py
@@ -20,9 +20,6 @@ class UseRegistrationForm(UserCreationForm):
                   ]
 
 
-
-
-
 class UserUpdateForm(forms.ModelForm):
     username = forms.CharField(max_length=20, required=False)
 
@@ -33,6 +30,33 @@ class UserUpdateForm(forms.ModelForm):
             'first_name',
             'last_name',
             'email',
+        ]
+
+
+class UserJobSetForm(forms.ModelForm):
+    Company_Name = forms.CharField()
+    Title = forms.CharField()
+    Work_Type = forms.CharField()
+    Description = forms.CharField()
+    Requirements = forms.CharField()
+    Experience = forms.CharField(required=False)
+    Salary = forms.IntegerField()
+    Location = forms.CharField(required=False)
+    Vacancy = forms.CharField(required=False)
+
+    class Meta:
+        model = PostWork
+        fields = [
+            'Company_Name',
+            'Title',
+            'Work_Type',
+            'Description',
+            'Requirements',
+            'Experience',
+            'Salary',
+            'Location',
+            'Vacancy',
+            'user',
         ]
 
 
@@ -51,9 +75,3 @@ class ProfileUpdateFrom(forms.ModelForm):
             'phone',
             'profile_picture',
         ]
-
-
-
-
-
-
